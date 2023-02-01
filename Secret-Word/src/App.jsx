4798c38f912c7daf377295bb0bd85a0384530b29
William Fernandes Pereira <williamfernandesp@outlook.com>
2023-02-01 15:10:53 -0300

    funções navegação SecretWord

--- Secret-Word/src/App.jsx
+++ Secret-Word/src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from "react";
+// React
+import { useState, useCallback, useEffect } from "react";
+
+// CSS
 import "./App.css";
+
+// Data
+import { wordsList } from "./data/words";
+
+// components
 import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  // começa o jogo
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  //processa a letra inserida
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  //restart do jogo
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
